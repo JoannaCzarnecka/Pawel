aa2c70a50dd98fdeca468583c6f3b2d1dfca10a5
unknown <student7@KIE-44662.wzr.local>
2015-01-22 14:36:23 +0100

    Initial commit

---
+++ app/src/main/java/com/example/student7/joanna/MyActivity.java
@@ -0,0 +1,25 @@
+package com.example.student7.joanna;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_my)
+public class MyActivity extends ActionBarActivity {
+
+    @Click(R.id.lista)
+        void startClicked(){
+
+        startActivity(new Intent(this, lista.class));
+    }
+@Click(R.id.zaloguj)
+    void loginClicked(){
+
+    startActivity(new Intent(this, zaloguj.class));
+}
+}
